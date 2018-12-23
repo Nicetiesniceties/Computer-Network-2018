04bcdf8b44ecfebd7798e0add8860242ae7a6f4c
Njceties <Njceties@NjcetiesdeMacBook-Air.local>
2018-12-23 18:25:14 +0800

    Finishing config reading for client init()

--- src/client.c
+++ src/client.c
@@ -1,5 +1,5 @@
 #include "client.h"
-struct socket_information *read_server_info(int mode)
+struct socket_information *read_server_info(int mode)//{{{
 {
 	struct socket_information *server =
 	  (struct socket_information *) malloc(sizeof(struct socket_information));
@@ -7,24 +7,41 @@ struct socket_information *read_server_info(int mode)
 	//mode 1: reading standard input
 	switch(mode)
 	{
-		case 1:
-		{
-			scanf("Please input the server ip: %s", server->ip);
-			scanf("Please input the server port: %d", &(server->port));
-			break;
-		}
 		case 0:
 		{
 			FILE * cfg_file = fopen("../config/client.cfg", "r");
 			char c;
 			if (cfg_file) 
 			{
+				int count_equal = 0;
+				char temp[20];
 				while ((c = getc(cfg_file)) != EOF)
-					putchar(c);
+					if(c == '=')
+					{
+						count_equal++;
+						if(count_equal == 2)
+							fscanf(cfg_file, "%s", server->ip);
+						else if(count_equal == 3)
+						{
+							fscanf(cfg_file, "%s", temp);
+							server->port = atoi(temp);
+						}
+					}
+					//putchar(c);
 				fclose(cfg_file);
+				fprintf(stdout, "Got it, server ip:%s, port: %d", server->ip, server->port);
 			}
 			else
 				fprintf(stderr, "Fail to read read client.cfg.\n");
+			break;
+		}
+		case 1:
+		{
+			fprintf(stdout, "Please input the server ip: ");
+			fscanf(stdin, "%s", server->ip);
+			fprintf(stdout, "Please input the server port: ");
+			fscanf(stdin, "%d", &(server->port));
+			fprintf(stdout, "Got it, server ip:%s, port: %d", server->ip, server->port);
 			break;
 		}
 		default:
@@ -34,11 +51,9 @@ struct socket_information *read_server_info(int mode)
 		}
 	}
 	return server;
-}
-int client_init()
+}//}}}
+int client_init()//{{{
 {
-	int mode = 0;
-	scanf("%d", &mode);
-	struct socket_information *server = read_server_info(mode);
+	struct socket_information *server = read_server_info(0);
 	return 0;
-}
+}//}}}
