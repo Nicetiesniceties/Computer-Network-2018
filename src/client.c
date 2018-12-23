7024f0a0e1e01bd3ef116a64f2317011d9c04fae
Njceties <Njceties@NjcetiesdeMacBook-Air.local>
2018-12-23 22:36:29 +0800

    Adding start connect for client_init

--- src/client.c
+++ src/client.c
@@ -29,7 +29,6 @@ struct socket_information *read_server_info(int mode)//{{{
 					}
 					//putchar(c);
 				fclose(cfg_file);
-				fprintf(stdout, "Got it, server ip:%s, port: %d", server->ip, server->port);
 			}
 			else
 				fprintf(stderr, "Fail to read read client.cfg.\n");
@@ -41,7 +40,6 @@ struct socket_information *read_server_info(int mode)//{{{
 			fscanf(stdin, "%s", server->ip);
 			fprintf(stdout, "Please input the server port: ");
 			fscanf(stdin, "%d", &(server->port));
-			fprintf(stdout, "Got it, server ip:%s, port: %d", server->ip, server->port);
 			break;
 		}
 		default:
@@ -50,10 +48,59 @@ struct socket_information *read_server_info(int mode)//{{{
 			break;
 		}
 	}
+	fprintf(stdout, "Got it, server is at %s:%d", server->ip, server->port);
 	return server;
 }//}}}
+int client_main_menu()//{{{
+{
+	int opt = 0;
+	char login_opt[20];
+	puts("> You are at login hall!\n");
+	puts("> How can I help you? login/signup/exit\n");
+	fscanf(stdin, "%s", login_opt);
+	//scanf("%d");
+	return opt;
+}//}}}
+
+int start_connect(struct socket_information *server)//{{{
+{
+	int sockfd = 0;
+	sockfd = socket(AF_INET , SOCK_STREAM , 0);
+	# ifdef debug
+	if (sockfd == -1)
+		fprintf(stderr, "Fail to create a socket!\n");
+	# endif
+	struct sockaddr_in info;
+	
+	/* socket setting */
+	memset(&info, 0, sizeof(info));//初始化，將struct涵蓋的bits設為0
+	info.sin_family = PF_INET;//sockaddr_in為Ipv4結構
+
+	/* localhost setting */
+	info.sin_addr.s_addr = inet_addr(server -> ip);//IP address
+	info.sin_port = htons(server -> port);
+	
+	/* connect! */
+	//printf("%d", connect(sockfd, (struct sockaddr *)&info, sizeof(info)));
+	int connect_status = connect(sockfd, (struct sockaddr *)&info, sizeof(info));
+	# ifdef debug
+	if(connect_status == -1)
+		fprintf(stderr, "Connection Error!\n");
+	# endif
+	return sockfd;
+}//}}}
+
 int client_init()//{{{
 {
 	struct socket_information *server = read_server_info(0);
+	//int sockfd = start_connect(server);
 	return 0;
 }//}}}
+int client_run()//{{{
+{
+	while(1)
+	{
+		int opt = client_main_menu();
+	}
+	return 0;
+}
