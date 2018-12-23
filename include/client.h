2f5f555a20ca95efbcb90520a86553ddd76440a6
Njceties <Njceties@NjcetiesdeMacBook-Air.local>
2018-12-23 17:15:34 +0800

    Adding possible include header

--- include/client.h
+++ include/client.h
@@ -1,2 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <time.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <time.h>
+#include <pthread.h>
